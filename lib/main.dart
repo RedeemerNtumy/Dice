3a6abd90f5709ccecc087f43ba5906aced493ba4
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-07-15 21:38:19 +0000

    First commits for this project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Dicey());
+}
+
+class Dicey extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold()
+      );
+  }
+}
